9e18fd38b0cdfef78d1c82befa63ea64e66a9367
XXXLQBXXXSTD <luongquocbao282006@gmail.com>
2026-03-18 09:06:05 +0700

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Detail } from './detail/detail';
+import { ProductsList } from './products-list/products-list';
+import { About } from './about/about';
 
 export const routes: Routes = [
-   {path:'',component:Home},
-   {path:'Detail',component:Detail},
-   {path: '**', redirectTo: "home", pathMatch: "full" },
+  { path: '', component: Home },
+  { path: 'products', component: ProductsList },
+  { path: 'about', component: About },
+  { path: 'Detail', component: Detail },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
-
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
-import { About } from './about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer,About],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
